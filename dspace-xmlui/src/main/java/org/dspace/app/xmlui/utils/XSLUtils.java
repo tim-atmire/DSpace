2799f17d0b486b48c7137880f7c77bb5e8e790e7
Tim Van den Langenbergh <tim@atmire.com>
2015-12-11 12:00:21 +0100

    Changed check for correct display and added logging for SQLExceptions. Removed the change to SimpleSearch, otherwise the contains filter would be used/shown instead of authority in the advanced search filters. Edited class ConfigurableBrowse to fix the browse page header.

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/utils/XSLUtils.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/utils/XSLUtils.java
@@ -7,11 +7,14 @@
  */
 package org.dspace.app.xmlui.utils;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.dspace.authority.AuthorityValue;
 import org.dspace.authority.AuthorityValueFinder;
 import org.dspace.core.Context;
 
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Utilities that are needed in XSL transformations.
@@ -19,6 +22,7 @@ import java.sql.SQLException;
  * @author Art Lowel (art dot lowel at atmire dot com)
  */
 public class XSLUtils {
+	private static final Logger log = Logger.getLogger(XSLUtils.class);
 
     /*
      * Cuts off the string at the space nearest to the targetLength if there is one within
@@ -60,19 +64,29 @@ public class XSLUtils {
 
     }
 
-	public static String getDiscoveryFilterDisplay(String display) throws SQLException {
-		Context ctx = new Context();
+	public static String getDiscoveryFilterDisplay(String display) {
+		Context ctx = null;
 
-		if (display != null) {
-			if (display.length() == 36 && display.contains("-")) {
+		try {
+			ctx = new Context();
+			AuthorityValueFinder avf = new AuthorityValueFinder();
+
+			if (StringUtils.isNotBlank(display)) {
+				List<AuthorityValue> avs = avf.findAll(ctx);
 				AuthorityValue authorityValue = new AuthorityValueFinder().findByUID(ctx, display);
-				if (authorityValue != null) {
+				if (authorityValue != null && avs.contains(authorityValue)) {
 					display = authorityValue.getValue();
 				}
 			}
 		}
-
-		ctx.complete();
+		catch(SQLException sqle) {
+			log.error(sqle.getLocalizedMessage(), sqle);
+		}
+		finally {
+			if(ctx != null) {
+				ctx.abort();
+			}
+		}
 
 		return display;
 	}
